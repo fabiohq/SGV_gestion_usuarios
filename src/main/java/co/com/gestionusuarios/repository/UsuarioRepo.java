d2912ac252d2ded3c70dfa4d6f4f137cc954dc58
Fabio <fabio12hq@hotmail.com>
2021-02-12 01:06:26 -0500

    Ajuste capa repositorio

--- src/main/java/co/com/gestionusuarios/repository/UsuarioRepo.java
+++ src/main/java/co/com/gestionusuarios/repository/UsuarioRepo.java
@@ -1,11 +1,8 @@
 package co.com.gestionusuarios.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import co.com.gestionusuarios.entity.Usuario;
 
-@Repository
-public class UsuarioRepo implements JpaRepository<Usuario, String>{
+public interface UsuarioRepo extends JpaRepository<Usuario, String>{
 
 }
