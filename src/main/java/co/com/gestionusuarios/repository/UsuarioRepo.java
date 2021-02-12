43487a436a0408311e8563cd17cc1d5c59293fc2
Fabio <fabio12hq@hotmail.com>
2021-02-12 01:19:40 -0500

    Prueba

--- src/main/java/co/com/gestionusuarios/repository/UsuarioRepo.java
+++ src/main/java/co/com/gestionusuarios/repository/UsuarioRepo.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import co.com.gestionusuarios.entity.Usuario;
 
 public interface UsuarioRepo extends JpaRepository<Usuario, String>{
-
+uiut
 }
