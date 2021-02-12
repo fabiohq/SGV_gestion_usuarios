1d750c3716279475bedf093aaee98d5140b94431
Fabio <fabio12hq@hotmail.com>
2021-02-12 00:00:26 -0500

    commit1 12022021

---
+++ src/main/java/co/com/gestionusuarios/SgvGestionUsuariosApplication.java
@@ -0,0 +1,13 @@
+package co.com.gestionusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvGestionUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvGestionUsuariosApplication.class, args);
+	}
+
+}
