c09b2d18736a9b35017d5a7ecbfc96e2ad222bdf
Fabio <fabio12hq@hotmail.com>
2021-02-18 23:09:58 -0500

    Commit 3-18-Feb-2021

--- src/main/java/co/com/gestionusuarios/repository/UsuarioRepo.java
+++
@@ -1,8 +0,0 @@
-package co.com.gestionusuarios.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.com.gestionusuarios.entity.Usuario;
-
-public interface UsuarioRepo extends JpaRepository<Usuario, String>{
-uiut
-}
